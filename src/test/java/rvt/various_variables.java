e55cbe6f1ece27b5943f22a2a67f3013f48c268c
24DP2AAdam <A240801AA@rvt.lv>
2025-09-10 13:13:52 +0300

    Various_variables

---
+++ src/test/java/rvt/various_variables.java
@@ -0,0 +1,23 @@
+package rvt;
+
+public class various_variables {
+    
+    public static void main(String[] args) {
+        int chickenAmout = 9000;
+        double Bacon = 0.1;
+        String tractorName = "Zetor";
+
+        System.out.println("Chicken:");
+        System.out.println(chickenAmout);
+        System.out.println("Bacon (kg):");
+        System.out.println(Bacon);
+        System.out.println("Tractor");
+        System.out.println(tractorName);
+
+        System.out.println("And finally, a summary:");
+        System.out.println(chickenAmout);
+        System.out.println(Bacon);
+        System.out.println(tractorName);
+
+    }
+}
